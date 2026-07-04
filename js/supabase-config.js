4731ed7c895384a0b29b9e9efeb4a4a367b745a2
matokee625 <matokee625@users.noreply.github.com>
2026-07-04 21:17:34 +0300

    Update Supabase anon key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,5 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   'https://hcnnimmosapaiwvthvta.supabase.co',
-  'YOUR_ANON_KEY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjbm5pbW1vc2FwYWl3dnRodnRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNzcxMDAsImV4cCI6MjA5ODY1MzEwMH0.BFVr1JKiQPxoOcEJryFItBJ89WxbeCmtCWBAKwv8cjg'
 );
